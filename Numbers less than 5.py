a8a19acaf582f11215382bb600c358bc707a6431
Ken <mulilamwendwa@gmail.com>
2021-06-08 12:00:25 +0300

    Update Numbers less than 5.py
    Changed 13 to 12

--- Numbers less than 5.py
+++ Numbers less than 5.py
@@ -2,7 +2,7 @@
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 #write a program that prints out all the elements of the list that are less than 5
 # Solution
-the_list = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+the_list = [1, 1, 2, 3, 5, 8, 12, 21, 34, 55, 89]
 for element in the_list:
     if(int(element) <5):
         print(str(element))
